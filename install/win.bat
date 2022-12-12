c85764b52abbb0555dab3a95555980bff8427b23
zx9597446 <zx9597446@gmail.com>
2022-12-12 15:54:45 +0800

    Create win.bat

---
+++ install/win.bat
@@ -0,0 +1,8 @@
+scoop install googlechrome
+scoop install firefox
+scoop install everything
+scoop install powertoys
+scoop install windows-terminal
+scoop install clink
+scoop install clink-completions
+scoop install vscode
