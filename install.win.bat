cc9d0c8104f6a30f2308c38aac1d2f15a69403d1
zx9597446 <zx9597446@gmail.com>
2022-12-12 15:55:30 +0800

    Delete install.win.bat

--- install.win.bat
+++
@@ -1,9 +0,0 @@
-scoop install googlechrome
-scoop install firefox
-scoop install everything
-scoop install powertoys
-scoop install windows-terminal
-scoop install clink
-scoop install clink-completions
-scoop install vscode
-scoop install adb
