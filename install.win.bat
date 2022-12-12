ab03da34d73d72b287f22a7878630d6333e485b1
zx9597446 <zx9597446@gmail.com>
2022-12-12 15:50:30 +0800

    Update install.win.bat

--- install.win.bat
+++ install.win.bat
@@ -1,1 +1,9 @@
-
+scoop install googlechrome
+scoop install firefox
+scoop install everything
+scoop install powertoys
+scoop install windows-terminal
+scoop install clink
+scoop install clink-completions
+scoop install vscode
+scoop install adb
