95f05386fea6240e7c77e18e7bfcfd83190ebacf
zx9597446 <zx9597446@gmail.com>
2023-01-17 12:09:57 +0800

    Update win.bat

--- install/win.bat
+++ install/win.bat
@@ -1,10 +1,10 @@
+scoop install clink
+scoop install clink-completions
 scoop install googlechrome
 scoop install firefox
 scoop install everything
 scoop install powertoys
 scoop install windows-terminal
-scoop install clink
-scoop install clink-completions
 scoop install vscode
 scoop install sysinternals
 scoop install fzf
@@ -12,5 +12,7 @@ scoop install ag
 scoop install croc
 scoop install jq
 scoop install mremoteng
+scoop install snipaste
+scoop install 7zip
 
 clink set clink.autostart doskey /macrofile=%userprofile%\doskey.macros
