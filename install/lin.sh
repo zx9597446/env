13e22bf04905cccb16171ded5a857fd6d7ee7f2c
zx9597446 <zx9597446@gmail.com>
2023-02-01 11:14:58 +0800

    Update lin.sh

--- install/lin.sh
+++ install/lin.sh
@@ -1,5 +1,4 @@
 apt --yes install git fish 
 echo /usr/local/bin/fish | sudo tee -a /etc/shells
 chsh -s /usr/local/bin/fish
-git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
-~/.fzf/install
+curl https://raw.githubusercontent.com/junegunn/fzf/master/install | bash
