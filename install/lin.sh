48f56466c4c05f044c87d908e8543297e906c517
zx9597446 <zx9597446@gmail.com>
2023-02-06 13:19:11 +0800

    Update lin.sh

--- install/lin.sh
+++ install/lin.sh
@@ -1,4 +1,4 @@
-apt --yes install git fish 
-echo /usr/local/bin/fish | sudo tee -a /etc/shells
-chsh -s /usr/local/bin/fish
+sudo apt-add-repository --yes ppa:fish-shell/release-3 && sudo apt update && sudo apt --yes install fish
+echo $(which fish) | sudo tee -a /etc/shells
+chsh -s $(which fish)
 curl https://raw.githubusercontent.com/junegunn/fzf/master/install | bash
