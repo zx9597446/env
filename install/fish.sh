5c2d3c39d98e6a5f0536466261d71774ce833c33
zx9597446 <zx9597446@gmail.com>
2023-02-23 16:05:44 +0800

    Update and rename lin.sh to fish.sh

--- install/fish.sh
+++ install/fish.sh
@@ -1,4 +1,3 @@
 sudo apt-add-repository --yes ppa:fish-shell/release-3 && sudo apt update && sudo apt --yes install fish
 echo $(which fish) | sudo tee -a /etc/shells
 chsh -s $(which fish)
-curl https://raw.githubusercontent.com/junegunn/fzf/master/install | bash
