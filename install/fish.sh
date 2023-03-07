69017f865e27348ab5b58ec4ee44f5399b43169c
zx9597446 <zx9597446@gmail.com>
2023-03-07 16:35:08 +0800

    Update fish.sh

--- install/fish.sh
+++ install/fish.sh
@@ -1,3 +1,8 @@
+# install nami in bash
+curl https://bash.ooo/nami.sh | bash
+nami install jinbe joker zoro brook
+
+# install fish
 sudo apt-add-repository -y ppa:fish-shell/release-3 && sudo apt update && sudo apt -y install fish
 echo $(which fish) | sudo tee -a /etc/shells
 chsh -s $(which fish)
@@ -5,8 +10,5 @@ chsh -s $(which fish)
 git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf && ~/.fzf/install --all
 
 curl https://getcroc.schollz.com | bash
-
-curl https://bash.ooo/nami.sh | bash
-nami install jinbe joker zoro brook
 
 apt update && apt install -y nodejs npm && npm install pm2 -g && pm2 startup
