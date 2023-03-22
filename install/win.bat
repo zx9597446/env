da78ca5e480f90c772d642f2e3b901b118e01026
zx9597446 <zx9597446@gmail.com>
2023-03-22 14:03:23 +0800

    Update win.bat

--- install/win.bat
+++ install/win.bat
@@ -1,20 +1,5 @@
-scoop install clink
-scoop install clink-completions
-scoop install curl
-scoop install googlechrome
-scoop install firefox
-scoop install everything
-scoop install powertoys
-scoop install windows-terminal
-scoop install vscode
-scoop install sysinternals
-scoop install fzf
-scoop install ag
-scoop install croc
-scoop install jq
-scoop install mremoteng
-scoop install snipaste
-scoop install 7zip
+scoop install clink clink-completions curl googlechrome firefox everything powertoys windows-terminal fzf ag croc mremoteng jq snipaste 7zip cpu-z gpu-z git-credential-manager
 
 clink set clink.autostart doskey /macrofile=%userprofile%\doskey.macros
 curl -o %localappdata%\clink\fzf.lua https://raw.githubusercontent.com/chrisant996/clink-fzf/main/fzf.lua
+clink set fzf.default_bindings true
