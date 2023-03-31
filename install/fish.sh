82feccd7ca06ad700272b3a2a2135df1f481f03b
zx9597446 <zx9597446@gmail.com>
2023-03-31 15:24:10 +0800

    Update fish.sh

--- install/fish.sh
+++ install/fish.sh
@@ -12,3 +12,6 @@ git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf && ~/.fzf/install
 curl https://getcroc.schollz.com | bash
 
 apt update && apt install -y nodejs npm && npm install pm2 -g && pm2 startup
+
+curl -sL https://raw.githubusercontent.com/jorgebucaran/fisher/main/functions/fisher.fish | source && fisher install jorgebucaran/fisher
+fisher install ankitsumitg/docker-fish-completions
