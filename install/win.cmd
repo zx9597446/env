61aacb2869b199d1c1dda2eb1ac43bf7068ae1d5
zx9597446 <zx9597446@gmail.com>
2023-04-04 16:04:16 +0800

    Update win.cmd

--- install/win.cmd
+++ install/win.cmd
@@ -2,5 +2,7 @@ scoop install clink clink-completions curl googlechrome firefox everything power
 
 clink set clink.autostart doskey /macrofile=%userprofile%\doskey.macros
 curl -o %localappdata%\clink\fzf.lua https://raw.githubusercontent.com/chrisant996/clink-fzf/main/fzf.lua
+curl -o %localappdata%\clink\more-clink-completions.lua https://github.com/sebthom/more-clink-completions/releases/download/latest-v2/more-clink-completions.lua
+
 clink set fzf.default_bindings true
 clink set cmd.auto_answer answer_yes
