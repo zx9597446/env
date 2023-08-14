8e8762b0c3a76cf7582a7812928e9bfaf3ab304b
zx9597446 <zx9597446@gmail.com>
2023-08-14 14:10:42 +0800

    Delete ntfy.sh

--- install/ntfy.sh
+++
@@ -1,5 +0,0 @@
-wget https://github.com/binwiederhier/ntfy/releases/download/v2.6.2/ntfy_2.6.2_linux_amd64.deb
-sudo dpkg -i ntfy_*.deb
-sudo systemctl stop ntfy
-sudo systemctl disable ntfy
-rm ntfy_*.deb
