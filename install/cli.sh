d0732dad1b017dc5e14b0ed1e2393a35d6983acf
zx9597446 <zx9597446@gmail.com>
2023-08-14 12:41:13 +0800

    Update cli.sh

--- install/cli.sh
+++ install/cli.sh
@@ -5,3 +5,6 @@ sudo apt install --yes jq ripgrep silversearcher-ag bat fd-find exa duf
 
 curl https://getcroc.schollz.com | bash
 safe-rm -rf .fzf && git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf && ~/.fzf/install --all
+
+wget https://github.com/joehillen/sysz/releases/latest/download/sysz && chmod +x sysz
+sudo mv sysz /usr/local/bin/
