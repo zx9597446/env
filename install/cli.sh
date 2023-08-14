dd8f270b8411c912c1ff46c33613c121a569dd7c
zx9597446 <zx9597446@gmail.com>
2023-08-14 14:24:56 +0800

    Update cli.sh

--- install/cli.sh
+++ install/cli.sh
@@ -1,7 +1,7 @@
 sudo apt update
 sudo apt install --yes git curl telnet moreutils safe-rm fail2ban unattended-upgrades
 sudo apt install --yes nodejs npm && npm install pm2 -g && pm2 startup
-sudo apt install --yes jq ripgrep silversearcher-ag bat fd-find exa duf
+sudo apt install --yes jq ripgrep silversearcher-ag rust-bat fd-find exa duf
 
 curl https://getcroc.schollz.com | bash
 safe-rm -rf .fzf && git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf && ~/.fzf/install --all
