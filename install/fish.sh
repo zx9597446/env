19caed1bfe4925971237ae9e3f586c50ac881984
zx9597446 <zx9597446@gmail.com>
2023-08-14 10:38:44 +0800

    Delete fish.sh

--- install/fish.sh
+++
@@ -1,7 +0,0 @@
-# install fish
-sudo apt-add-repository -y ppa:fish-shell/release-3 && sudo apt update && sudo apt -y install fish
-echo $(which fish) | sudo tee -a /etc/shells
-chsh -s $(which fish)
-
-curl -sL https://raw.githubusercontent.com/jorgebucaran/fisher/main/functions/fisher.fish | source && fisher install jorgebucaran/fisher
-fisher install ankitsumitg/docker-fish-completions
