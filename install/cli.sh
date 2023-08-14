14efcc2bdcd6697c098aa8efb90f144925b52839
zx9597446 <zx9597446@gmail.com>
2023-08-14 14:20:08 +0800

    Update cli.sh

--- install/cli.sh
+++ install/cli.sh
@@ -1,5 +1,5 @@
 sudo apt update
-sudo apt install --yes git curl telnet moreutils safe-rm fail2ban unattended-upgrades apt-listchanges
+sudo apt install --yes git curl telnet moreutils safe-rm fail2ban unattended-upgrades
 sudo apt install --yes nodejs npm && npm install pm2 -g && pm2 startup
 sudo apt install --yes jq ripgrep silversearcher-ag bat fd-find exa duf
 
