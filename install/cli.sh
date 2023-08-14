b5b2d1b05be7650dd32dfcf4b86bb305bf2bf380
zx9597446 <zx9597446@gmail.com>
2023-08-14 15:12:39 +0800

    Update cli.sh

--- install/cli.sh
+++ install/cli.sh
@@ -1,7 +1,33 @@
-sudo apt update
+sudo apt update && sudo apt upgrade
 sudo apt install --yes git curl telnet moreutils safe-rm fail2ban unattended-upgrades
+
+cat <<EOF > "/etc/apt/apt.conf.d/20auto-upgrades"
+APT::Periodic::Update-Package-Lists "1";
+APT::Periodic::Unattended-Upgrade "1";
+APT::Periodic::AutocleanInterval "7";
+EOF
+sudo systemctl enable unattended-upgrades
+sudo systemctl restart unattended-upgrades
+
+sudo touch /etc/fail2ban/jail.local
+cat <<EOF > "/etc/fail2ban/jail.local"
+enabled = true
+port    = ssh
+filter  = sshd
+logpath  = /var/log/auth.log
+maxretry = 5
+findtime  = 60m
+bantime   = 60m
+EOF
+sudo systemctl enable fail2ban
+sudo systemctl restart fail2ban.service
+
+
 sudo apt install --yes nodejs npm && npm install pm2 -g && pm2 startup
 sudo apt install --yes jq ripgrep silversearcher-ag bat fd-find exa duf
+
+sudo mv /usr/bin/batcat /usr/bin/bat
+sudo mv /usr/bin/fdfind /usr/bin/fd
 
 curl https://getcroc.schollz.com | bash
 safe-rm -rf .fzf && git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf && ~/.fzf/install --all
