830741c46d33eff6a5f54d82b2550a1e0e82f269
zx9597446 <zx9597446@gmail.com>
2023-08-14 18:26:02 +0800

    Update sys.sh

--- install/sys.sh
+++ install/sys.sh
@@ -11,6 +11,8 @@ sudo systemctl restart unattended-upgrades
 sudo systemctl status unattended-upgrades
 
 sudo apt install --yes nodejs npm && npm install pm2 -g && pm2 startup
+sudo apt install --yes docker docker-compose
+
 sudo apt install --yes jq ripgrep silversearcher-ag bat fd-find exa duf
 
 sudo mv /usr/bin/batcat /usr/bin/bat
@@ -27,3 +29,15 @@ wget https://github.com/joehillen/sysz/releases/latest/download/sysz && chmod +x
 sudo mv sysz /usr/local/bin/
 
 curl https://raw.githubusercontent.com/jesseduffield/lazydocker/master/scripts/install_update_linux.sh | DIR=/usr/local/bin sudo bash
+
+wget https://github.com/binwiederhier/ntfy/releases/download/v2.6.2/ntfy_2.6.2_linux_amd64.deb
+sudo dpkg -i ntfy_*.deb
+sudo systemctl stop ntfy
+sudo systemctl disable ntfy
+rm ntfy_*.deb
+
+curl -L -O https://artifacts.elastic.co/downloads/beats/filebeat/filebeat-8.9.0-amd64.deb
+sudo dpkg -i filebeat-*.deb
+sudo systemctl stop filebeat
+sudo systemctl enable filebeat
+rm filebeat-*.deb
