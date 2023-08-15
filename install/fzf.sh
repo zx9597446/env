3b92b3679e210d63e652c871c99c109616829d48
zx9597446 <zx9597446@gmail.com>
2023-08-15 11:02:01 +0800

    Update fzf.sh

--- install/fzf.sh
+++ install/fzf.sh
@@ -1,1 +1,1 @@
-safe-rm -rf ~/.fzf && git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf && ~/.fzf/install --all
+safe-rm -rf ~/.fzf && git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf && ~/.fzf/install --all && source ~/.bashrc
