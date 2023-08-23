b3f9017db75ca31246157702786701acbede160c
zx9597446 <zx9597446@gmail.com>
2023-08-23 16:23:08 +0800

    Update cli.sh

--- install/cli.sh
+++ install/cli.sh
@@ -2,12 +2,12 @@ sudo apt update
 sudo apt install --yes git git-extras vim curl wget telnet screen moreutils coreutils safe-rm 
 sudo apt install --yes jq ripgrep silversearcher-ag retry
 
-retry -- curl --max-time 60 https://getcroc.schollz.com | retry bash
+safe-rm -rf ~/.fzf && git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf && ~/.fzf/install --all
 
-retry -- wget --timeout=60 https://github.com/zx9597446/qtunnel/releases/download/v0.0.1/qtunnel-linux-amd64 && chmod +x qtunnel-linux-amd64 && sudo mv qtunnel-linux-amd64 /usr/local/bin/qtunnel
+curl https://getcroc.schollz.com | bash
 
-retry -- wget --timeout=60 https://github.com/joehillen/sysz/releases/latest/download/sysz && chmod +x sysz && sudo mv sysz /usr/local/bin/
+wget https://github.com/zx9597446/qtunnel/releases/download/v0.0.1/qtunnel-linux-amd64 && chmod +x qtunnel-linux-amd64 && sudo mv qtunnel-linux-amd64 /usr/local/bin/qtunnel
 
-retry -- curl --max-time 60 https://raw.githubusercontent.com/jesseduffield/lazydocker/master/scripts/install_update_linux.sh | DIR=/usr/local/bin sudo retry bash
+wget https://github.com/joehillen/sysz/releases/latest/download/sysz && chmod +x sysz && sudo mv sysz /usr/local/bin/
 
-safe-rm -rf ~/.fzf && retry -- git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf && retry -- bash ~/.fzf/install --all
+curl https://raw.githubusercontent.com/jesseduffield/lazydocker/master/scripts/install_update_linux.sh | DIR=/usr/local/bin sudo bash
