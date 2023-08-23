9aa5ad1af99e3012d2767268f94dc8c7b5e1fb02
zx9597446 <zx9597446@gmail.com>
2023-08-23 11:31:46 +0800

    Update cli.sh

--- install/cli.sh
+++ install/cli.sh
@@ -2,9 +2,7 @@ sudo apt update
 sudo apt install --yes git vim curl wget telnet screen moreutils coreutils safe-rm 
 sudo apt install --yes jq ripgrep silversearcher-ag retry
 
-sudo ln -s /usr/bin/batcat /usr/local/bin/bat
-sudo ln -s /usr/bin/fdfind /usr/local/bin/fd
-
+safe-rm -rf ~/.fzf && git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf && ~/.fzf/install --all
 curl https://getcroc.schollz.com | bash
 
 wget https://github.com/zx9597446/qtunnel/releases/download/v0.0.1/qtunnel-linux-amd64 && chmod +x qtunnel-linux-amd64
@@ -15,4 +13,3 @@ sudo mv sysz /usr/local/bin/
 
 curl https://raw.githubusercontent.com/jesseduffield/lazydocker/master/scripts/install_update_linux.sh | DIR=/usr/local/bin sudo bash
 
-safe-rm -rf ~/.fzf && git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf && ~/.fzf/install --all
