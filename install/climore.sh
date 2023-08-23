36b64e4e80a62c3a8498f9841d7d405707ed6bf5
zx9597446 <zx9597446@gmail.com>
2023-08-23 11:35:23 +0800

    Update climore.sh

--- install/climore.sh
+++ install/climore.sh
@@ -1,1 +1,9 @@
 
+# qtunnel
+wget https://github.com/zx9597446/qtunnel/releases/download/v0.0.1/qtunnel-linux-amd64 && chmod +x qtunnel-linux-amd64 && sudo mv qtunnel-linux-amd64 /usr/local/bin/qtunnel
+
+# sysz
+wget https://github.com/joehillen/sysz/releases/latest/download/sysz && chmod +x sysz && sudo mv sysz /usr/local/bin/
+
+# layz docker
+curl https://raw.githubusercontent.com/jesseduffield/lazydocker/master/scripts/install_update_linux.sh | DIR=/usr/local/bin sudo bash
