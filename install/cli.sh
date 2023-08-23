847d8aa0eadae84b17c64a15d95e8c7a3abb28f8
zx9597446 <zx9597446@gmail.com>
2023-08-23 12:10:38 +0800

    Update cli.sh

--- install/cli.sh
+++ install/cli.sh
@@ -1,7 +1,5 @@
 sudo apt update
-sudo apt install --yes git vim curl wget telnet screen moreutils coreutils safe-rm 
-sudo apt install --yes jq ripgrep silversearcher-ag retry
 
-safe-rm -rf ~/.fzf && git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf && ~/.fzf/install --all
+safe-rm -rf ~/.fzf && retry -- git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf && ~/.fzf/install --all
 
-curl https://getcroc.schollz.com | bash
+retry -- curl https://getcroc.schollz.com | bash
