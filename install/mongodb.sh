ea34dcc55534474f96b164d534ab53e7cd2b303a
zx9597446 <zx9597446@gmail.com>
2023-08-23 10:28:40 +0800

    Update mongodb.sh

--- install/mongodb.sh
+++ install/mongodb.sh
@@ -1,4 +1,4 @@
-curl -fsSL https://pgp.mongodb.com/server-7.0.asc | sudo gpg -o /usr/share/keyrings/mongodb-server-7.0.gpg --dearmor
+curl -fsSL https://pgp.mongodb.com/server-7.0.asc | sudo gpg -o /etc/apt/trusted.gpg.d/mongodb-server-7.0.gpg --dearmor
 
 echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu jammy/mongodb-org/7.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-7.0.list
 
