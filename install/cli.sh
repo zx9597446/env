e77772d44c8390c373312af4e07d2da30517c096
zx9597446 <zx9597446@gmail.com>
2023-08-23 15:40:38 +0800

    Update cli.sh

--- install/cli.sh
+++ install/cli.sh
@@ -2,7 +2,7 @@ sudo apt update
 sudo apt install --yes git git-extras vim curl wget telnet screen moreutils coreutils safe-rm 
 sudo apt install --yes jq ripgrep silversearcher-ag retry
 
-retry -- curl https://getcroc.schollz.com | retry bash -x
+retry -- curl https://getcroc.schollz.com | retry -- bash -x
 
 retry -- wget --timeout=30 https://github.com/zx9597446/qtunnel/releases/download/v0.0.1/qtunnel-linux-amd64 && chmod +x qtunnel-linux-amd64 && sudo mv qtunnel-linux-amd64 /usr/local/bin/qtunnel
 
