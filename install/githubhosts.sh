4b98378b130c7694b91b8404ea0799073a04e25b
zx9597446 <zx9597446@gmail.com>
2023-08-23 11:21:21 +0800

    Update githubhosts.sh

--- install/githubhosts.sh
+++ install/githubhosts.sh
@@ -1,1 +1,2 @@
-curl https://gitlab.com/ineo6/hosts/-/raw/master/next-hosts | sudo tee /etc/hosts
+sudo cp /etc/hosts /etc/hosts.bak
+curl https://gitlab.com/ineo6/hosts/-/raw/master/next-hosts | sudo tee -a /etc/hosts
