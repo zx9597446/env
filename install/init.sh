85a796e339bb6bdc36970c4fd89453dfad39e564
zx9597446 <zx9597446@gmail.com>
2023-08-23 14:51:01 +0800

    Update init.sh

--- install/init.sh
+++ install/init.sh
@@ -1,7 +1,7 @@
 sudo apt update && sudo apt upgrade --yes
 
-retry -- curl https://raw.githubusercontent.com/zx9597446/env/main/install/sec.sh | bash
+retry -- curl --max-time 60 https://raw.githubusercontent.com/zx9597446/env/main/install/sec.sh | bash
 
-retry -- curl https://raw.githubusercontent.com/zx9597446/env/main/install/sys.sh | bash
+retry -- curl --max-time 60 https://raw.githubusercontent.com/zx9597446/env/main/install/sys.sh | bash
 
-retry -- curl https://raw.githubusercontent.com/zx9597446/env/main/install/cli.sh | bash
+retry -- curl --max-time 60 https://raw.githubusercontent.com/zx9597446/env/main/install/cli.sh | bash
