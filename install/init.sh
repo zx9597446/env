d525e5cc80281187932c701c03690412b7262270
zx9597446 <zx9597446@gmail.com>
2023-08-24 11:32:38 +0800

    Update init.sh

--- install/init.sh
+++ install/init.sh
@@ -4,4 +4,4 @@ curl https://raw.githubusercontent.com/zx9597446/env/main/install/sec.sh | bash 
 
 curl https://raw.githubusercontent.com/zx9597446/env/main/install/sys.sh | bash -x
 
-curl https://raw.githubusercontent.com/zx9597446/env/main/install/cli.sh | bash -x
+curl --retry 100 --max-time 60 https://raw.githubusercontent.com/zx9597446/env/main/install/cli.sh | retry -- bash -ex
