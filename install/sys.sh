8a859cef0fb89e4c4e04a4083176158d13f3dd12
zx9597446 <zx9597446@gmail.com>
2023-08-24 14:02:54 +0800

    Update sys.sh

--- install/sys.sh
+++ install/sys.sh
@@ -18,7 +18,7 @@ sudo apt install caddy
 sudo systemctl stop caddy
 sudo systemctl disable caddy
 
-retry -- wget --timtout=60 https://github.com/binwiederhier/ntfy/releases/download/v2.6.2/ntfy_2.6.2_linux_amd64.deb
+retry -- wget https://github.com/binwiederhier/ntfy/releases/download/v2.6.2/ntfy_2.6.2_linux_amd64.deb
 sudo dpkg -i ntfy_*.deb
 sudo systemctl stop ntfy
 sudo systemctl disable ntfy
