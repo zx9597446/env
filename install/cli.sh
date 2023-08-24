dbe867f52a866527ce07a34da002e2e5cddd806d
zx9597446 <zx9597446@gmail.com>
2023-08-24 10:38:40 +0800

    Update cli.sh

--- install/cli.sh
+++ install/cli.sh
@@ -1,4 +1,4 @@
-sudo apt install --yes git vim curl wget telnet screen moreutils coreutils safe-rm retry
+sudo apt install --yes git vim curl wget telnet screen moreutils coreutils safe-rm retry proxychains4
 sudo apt install --yes jq ripgrep silversearcher-ag
 
 safe-rm -rf ~/.fzf && git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf && ~/.fzf/install --all
