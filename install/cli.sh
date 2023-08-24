b3650ce69f4ef5712b150308823c6d2497e35ce6
zx9597446 <zx9597446@gmail.com>
2023-08-24 11:42:47 +0800

    Update cli.sh

--- install/cli.sh
+++ install/cli.sh
@@ -1,9 +1,9 @@
 sudo apt install --yes git vim curl wget telnet screen moreutils coreutils safe-rm retry proxychains4
 sudo apt install --yes jq ripgrep silversearcher-ag
 
-safe-rm -rf ~/.fzf && git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf && ~/.fzf/install --all
-
 curl https://getcroc.schollz.com | bash
+
+safe-rm -rf ~/.fzf && git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf && ~/.fzf/install --all
 
 wget https://github.com/zx9597446/qtunnel/releases/download/v0.0.1/qtunnel-linux-amd64 && chmod +x qtunnel-linux-amd64 && sudo mv qtunnel-linux-amd64 /usr/local/bin/qtunnel
 
