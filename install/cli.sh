40cba1ccbfcc15b91ba3695cd62c956503f22ee0
zx9597446 <zx9597446@gmail.com>
2023-09-08 14:44:29 +0800

    Update cli.sh

--- install/cli.sh
+++ install/cli.sh
@@ -6,7 +6,7 @@ curl https://getcroc.schollz.com | bash
 
 safe-rm -rf ~/.fzf && git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf && ~/.fzf/install --all
 
-wget https://github.com/zx9597446/qtunnel/releases/latest/download/qtunnel-linux-amd64 && chmod +x qtunnel-linux-amd64 && sudo mv qtunnel-linux-amd64 /usr/local/bin/qtunnel
+wget https://github.com/zx9597446/qtunnel/releases/latest/download/qtunnel && chmod +x qtunnel && sudo mv qtunnel /usr/local/bin/qtunnel
 
 wget https://github.com/joehillen/sysz/releases/latest/download/sysz && chmod +x sysz && sudo mv sysz /usr/local/bin/
 
