2d6ae2905517462e167e1f25b86db32d2ad948b1
zx9597446 <zx9597446@gmail.com>
2023-09-08 11:00:07 +0800

    Update sys.sh

--- install/sys.sh
+++ install/sys.sh
@@ -18,7 +18,7 @@ sudo systemctl stop filebeat
 sudo systemctl enable filebeat
 rm filebeat-*.deb
 
-retry -- wget https://github.com/binwiederhier/ntfy/releases/download/v2.6.2/ntfy_2.6.2_linux_amd64.deb
+retry -- wget https://github.com/binwiederhier/ntfy/releases/download/v2.7.0/ntfy_2.7.0_linux_amd64.deb
 sudo dpkg -i ntfy_*.deb
 sudo systemctl stop ntfy
 sudo systemctl disable ntfy
