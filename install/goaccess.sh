7652ad3851363e4623df7e4e18b6cf899f7cd190
zx9597446 <zx9597446@gmail.com>
2024-01-15 11:00:37 +0800

    Update goaccess.sh

--- install/goaccess.sh
+++ install/goaccess.sh
@@ -2,3 +2,5 @@ wget -O - https://deb.goaccess.io/gnugpg.key | gpg --dearmor | sudo tee /usr/sha
 echo "deb [signed-by=/usr/share/keyrings/goaccess.gpg arch=$(dpkg --print-architecture)] https://deb.goaccess.io/ $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/goaccess.list
 sudo apt-get update
 sudo apt-get install goaccess
+
+# goaccess /var/log/caddy/access.log --log-format=CADDY
