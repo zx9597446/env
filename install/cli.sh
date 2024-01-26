c0b1a33d25a7804ec1a7a72139082e2301e7da08
zx9597446 <zx9597446@gmail.com>
2024-01-26 16:01:26 +0800

    Update cli.sh

--- install/cli.sh
+++ install/cli.sh
@@ -22,4 +22,4 @@ sudo eget dalance/procs --asset ^musl --to /usr/local/bin
 sudo eget --all fatedier/frp --to /usr/local/bin
 sudo eget ginuerzh/gost --to /usr/local/bin
 sudo eget codeskyblue/gohttpserver --to /usr/local/bin
-
+sudo eget svenstaro/miniserve --asset ^musl --to /usr/local/bin
